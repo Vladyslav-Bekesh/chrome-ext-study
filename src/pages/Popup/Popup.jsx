cfc9d26459e4b9ab8af0ce27a56d311d8dcd5520
Mykola <114566822+Vladyslav-Bekesh@users.noreply.github.com>
2024-06-05 17:30:55 +0300

    some changes

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
+  const getCurrentTab = async () => {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  };
+
+  const startObservingMessages = async () => {
+    const tab = await getCurrentTab();
+    const response = await chrome.tabs.sendMessage(
+      tab.id,
+      'hello from popup!!!'
+    );
+    console.log(response);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button
+          onClick={() => {
+            console.log('test');
+            startObservingMessages();
+            chrome.tabs.query();
+          }}
+        >
+          stop or start observing
+        </button>
+
         <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          It's finaly working
         </a>
       </header>
     </div>
